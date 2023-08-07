ade92ab156c48a6546639e4c6fbb31cc22e51ac4
SkeloGH <dev.aaron.gh@gmail.com>
2023-08-07 05:26:57 -0600

    updated lint config and included mandatory tests

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    'max-len': ['error', { code: 100 }],
+  }
+};

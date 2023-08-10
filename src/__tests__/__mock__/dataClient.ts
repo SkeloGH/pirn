6bdc20897cdd387874dc51b39125869b35019b07
SkeloGH <dev.aaron.gh@gmail.com>
2023-08-09 21:08:59 -0600

    make mock dataclient purely mocked

--- src/__tests__/__mock__/dataClient.ts
+++ src/__tests__/__mock__/dataClient.ts
@@ -1,4 +1,4 @@
-import { IDataClient, IIgnoreField, IIgnoreTable } from "interfaces";
+import { IDataClient } from "interfaces";
 import QueriesAPI from "api/queries";
 import IgnoresAPI from "api/ignores";
 
@@ -22,49 +22,25 @@ class MockDataClient implements IDataClient {
   public disconnect = jest.fn();
   
   public addQuery = jest.fn();
-  public addQueries = this.queriesAPI.addQueries;
-  public getQuery = this.queriesAPI.getQuery;
-  public getQueries = this.queriesAPI.getQueries;
-  public removeQuery = this.queriesAPI.removeQuery;
-  public removeQueries = this.queriesAPI.removeQueries;
+  public addQueries = jest.fn();
+  public getQuery = jest.fn();
+  public getQueries = jest.fn();
+  public removeQuery = jest.fn();
+  public removeQueries = jest.fn();
   
   // Field methods
-  public addIgnoreField = (ignoreField: IIgnoreField) => {
-    this.ignoresAPI.addIgnoreField(ignoreField.clientId, ignoreField.field);
-    return this.options.ignoreFields;
-  };
-  public addIgnoreFields = (ignoreFields: IIgnoreField[]) => {
-    ignoreFields.forEach(ignoreField => this.addIgnoreField(ignoreField));
-    return this.options.ignoreFields;
-  }
-  public removeIgnoreField = (ignoreFieldId: string) => {
-    this.ignoresAPI.removeIgnoreField(this.clientId, ignoreFieldId);
-    return this.options.ignoreFields;
-  }
-  public removeIgnoreFields = (ignoreFieldIds: string[]) => {
-    ignoreFieldIds.forEach(ignoreFieldId => this.removeIgnoreField(ignoreFieldId));
-    return this.options.ignoreFields;
-  }
-  public getIgnoreFields = () => this.options.ignoreFields;
+  public addIgnoreField = jest.fn();
+  public addIgnoreFields = jest.fn();
+  public removeIgnoreField = jest.fn();
+  public removeIgnoreFields = jest.fn();
+  public getIgnoreFields = jest.fn();
   
   // Table methods
-  public addIgnoreTable = (ignoreTable: IIgnoreTable) => {
-    this.ignoresAPI.addIgnoreTable(ignoreTable.clientId, ignoreTable.table);
-    return this.options.ignoreTables;
-  }
-  public addIgnoreTables = (ignoreTables: IIgnoreTable[]) => {
-    ignoreTables.forEach(ignoreTable => this.addIgnoreTable(ignoreTable));
-    return this.options.ignoreTables;
-  }
-  public removeIgnoreTable = (ignoreTableId: string) => {
-    this.ignoresAPI.removeIgnoreTable(this.clientId, ignoreTableId);
-    return this.options.ignoreTables;
-  }
-  public removeIgnoreTables = (ignoreTableIds: string[]) => {
-    ignoreTableIds.forEach(ignoreTableId => this.removeIgnoreTable(ignoreTableId));
-    return this.options.ignoreTables;
-  }
-  public getIgnoreTables = () => this.options.ignoreTables;
+  public addIgnoreTable = jest.fn();
+  public addIgnoreTables = jest.fn();
+  public removeIgnoreTable = jest.fn();
+  public removeIgnoreTables = jest.fn();
+  public getIgnoreTables = jest.fn();
 }
 
 export default MockDataClient;

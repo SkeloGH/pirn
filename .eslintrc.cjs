bcf23a54c856847ff35186e816bc7cf40758b30d
SkeloGH <dev.aaron.gh@gmail.com>
2023-08-14 17:09:47 -0600

    prevent test folders from being built, added a separate tsconfig specific for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
       ],
       extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
       parserOptions: {
-        project: ['packages/**/tsconfig.json'],
+        project: ['packages/**/tsconfig.eslint.json'],
         tsconfigRootDir: __dirname,
       },
       plugins: ['@typescript-eslint'],
